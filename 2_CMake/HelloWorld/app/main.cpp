f4f58c14fb7545405a131b41dd69fa6190ea251e
batu <bipci@myseneca.ca>
2022-07-30 17:17:08 -0400

    config

--- 2_CMake/HelloWorld/app/main.cpp
+++ 2_CMake/HelloWorld/app/main.cpp
@@ -1,8 +1,11 @@
+#include "config.hpp"
 #include "my_lib.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
 
+  std::cout << project_name << "\n"
+            << project_version << "\n";
   my_lib::MyClass my_class;
 
   // my_class.printHelloWorld();
